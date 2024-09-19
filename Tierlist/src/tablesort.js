5fff33c6250ca8e8e9b1779977276c4f709aed63
Daniele Grandi <danielegrandi01@gmail.com>
2024-09-19 22:30:12 +0200

    s

--- Tierlist/src/tablesort.js
+++ Tierlist/src/tablesort.js
@@ -30,9 +30,6 @@ function sortTableByColumn(table, column, asc = true) {
 	table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
 	table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
 	table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc);
-
-	table.querySelector(`td:nth-child(${column + 1})`).classList.add("selected");
-
 }
 document.querySelectorAll(".table-sortable th").forEach(headerCell => {
 	headerCell.addEventListener("click", () => {
