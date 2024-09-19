be6436529fe3e5c40c402f4b2a342ef889677cad
Daniele Grandi <danielegrandi01@gmail.com>
2024-09-19 20:56:31 +0200

    s

--- Tierlist/src/tablesort.js
+++ Tierlist/src/tablesort.js
@@ -30,6 +30,7 @@ function sortTableByColumn(table, column, asc = true) {
 	table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
 	table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
 	table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc);
+	table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("selected");
 }
 document.querySelectorAll(".table-sortable th").forEach(headerCell => {
 	headerCell.addEventListener("click", () => {
@@ -39,12 +40,4 @@ document.querySelectorAll(".table-sortable th").forEach(headerCell => {
 
 		sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
 	});
-});
-$('.table-sortable th').on('click', function() {
-    var $currentTable = $(this).closest('table');
-    var index = $(this).index();
-    $currentTable.find('td').removeClass('selected');
-    $currentTable.find('tr').each(function() {
-        $(this).find('td').eq(index).addClass('selected');
-    });
 });
