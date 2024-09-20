7d99c9780fe147c60a0c579f2018687a683be97e
Daniele Grandi <danielegrandi01@gmail.com>
2024-09-21 01:59:02 +0200

    s

--- Tierlist/src/tablesort.js
+++ Tierlist/src/tablesort.js
@@ -12,8 +12,8 @@ function sortTableByColumn(table, column, asc = true) {
 
 	// Sort each row
 	const sortedRows = rows.sort((a, b) => {
-		const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
-		const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+		const aColText = a.querySelector(td:nth-child(${column + 1})).textContent.trim();
+		const bColText = b.querySelector(td:nth-child(${column + 1})).textContent.trim();
 
 		return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
 	});
@@ -28,8 +28,8 @@ function sortTableByColumn(table, column, asc = true) {
 
 	// Remember how the column is currently sorted
 	table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
-	table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
-	table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc);
+	table.querySelector(th:nth-child(${column + 1})).classList.toggle("th-sort-asc", asc);
+	table.querySelector(th:nth-child(${column + 1})).classList.toggle("th-sort-desc", !asc);
 
 	// Remove the "selected" class from all previously selected columns
 	table.querySelectorAll("td").forEach(td => td.classList.remove("selected"));
@@ -37,7 +37,7 @@ function sortTableByColumn(table, column, asc = true) {
 
 	// Highlight the selected column
 	rows.forEach(row => {
-		const cell = row.querySelector(`td:nth-child(${column + 1})`);
+		const cell = row.querySelector(td:nth-child(${column + 1}));
 		if (cell) {
 			cell.classList.add("selected");
 		}
@@ -45,28 +45,12 @@ function sortTableByColumn(table, column, asc = true) {
 
 }
 
-
-let isFirstClickOnTier = true;  // Variabile per tracciare il primo click su Tier
-
 document.querySelectorAll(".table-sortable th").forEach(headerCell => {
 	headerCell.addEventListener("click", () => {
 		const tableElement = headerCell.parentElement.parentElement.parentElement;
 		const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
-		
-		// Controlla se è la colonna "Tier" (indice 0)
-		const isTierColumn = headerIndex === 0;
-		let currentIsAscending;
+		const currentIsAscending = headerCell.classList.contains("th-sort-asc");
 
-		if (isTierColumn && isFirstClickOnTier) {
-			// Se è il primo click su Tier, imposta l'ordinamento decrescente
-			currentIsAscending = false;  // Decrescente
-			isFirstClickOnTier = false;  // Disattiva il comportamento speciale dopo il primo click
-		} else {
-			// Comportamento normale (toggles tra ascendente e discendente)
-			currentIsAscending = headerCell.classList.contains("th-sort-desc");
-		}
-
-		// Chiamata alla funzione di ordinamento
 		sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
 	});
 });
