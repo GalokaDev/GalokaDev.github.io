a40fae9bc4344242f2c562503372e425f1a01ba4
Daniele Grandi <danielegrandi01@gmail.com>
2024-09-24 01:48:01 +0200

    s

---
+++ js/teambuilder.js
@@ -0,0 +1,6 @@
+// Funzione per calcolare la squadra (simulata per esempio)
+document.getElementById('calculate').addEventListener('click', function() {
+    // Esegui qui il calcolo basato sui valori inseriti dall'utente
+    let result = "La tua squadra è ben bilanciata!";
+    document.getElementById('result').innerText = result;
+});
