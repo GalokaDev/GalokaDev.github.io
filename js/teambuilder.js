17b6ad0e20626be44062b4bf95bcc959ab8bea32
Daniele Grandi <danielegrandi01@gmail.com>
2024-09-24 05:01:34 +0200

    s

--- js/teambuilder.js
+++ js/teambuilder.js
@@ -98,7 +98,7 @@ document.getElementById('calculate').addEventListener('click', function() {
     // Verifica dei tipi con punteggi superiori a 2
     let weaknesses = [];
     for (let type in typeScores) {
-        if (typeScores[type] > 2) {
+        if (typeScores[type] > 0) {
             weaknesses.push(type);
         }
     }
