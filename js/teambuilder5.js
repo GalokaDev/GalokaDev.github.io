a6b61e1c59264042ccc63b6c5657a302bc791772
Daniele Grandi <danielegrandi01@gmail.com>
2024-09-26 16:11:02 +0200

    s

--- js/teambuilder5.js
+++ js/teambuilder5.js
@@ -137,7 +137,7 @@ function calculateWeaknesses(team) {
         ghost: { weakTo: ['ghost', 'dark'], resists: ['poison', 'bug'], immuneTo: ['normal', 'fighting'] },
         dragon: { weakTo: ['ice', 'dragon'], resists: ['fire', 'water', 'electric', 'grass'], immuneTo: [] },
         dark: { weakTo: ['fighting', 'bug'], resists: ['ghost', 'dark'], immuneTo: ['psychic'] },
-        steel: { weakTo: ['fire', 'fighting', 'ground'], resists: ['normal', 'flying', 'rock', 'bug', 'steel', 'grass', 'ice', 'psychic', 'dragon','ghost','dark'], immuneTo: ['poison'] }
+        steel: { weakTo: ['fire', 'fighting', 'ground'], resists: ['normal', 'flying', 'rock', 'bug', 'steel', 'grass', 'ice', 'psychic', 'dragon', 'ghost', 'dark'], immuneTo: ['poison'] }
     };
 
     const typeWeaknessChart = {
@@ -189,6 +189,8 @@ function calculateWeaknesses(team) {
                     typeWeaknessChart[t] -= 2;
                 });
             }
+        } else {
+            console.warn(`Pokémon "${pokemon.name}" non trovato in pokemonRoles.`);
         }
     });
 
