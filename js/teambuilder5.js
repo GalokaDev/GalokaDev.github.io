70563704cb4602f2bbe568539bfc76385f5958c0
Daniele Grandi <danielegrandi01@gmail.com>
2024-09-27 03:45:09 +0200

    s

--- js/teambuilder5.js
+++ js/teambuilder5.js
@@ -408,15 +408,6 @@ document.getElementById('calculate').addEventListener('click', function() {
 
 
 
-    // Dopo aver determinato il bestModel, aggiungi il bonus se è "rain" e il team ha un rainSetter
-    let hasRainSetter = team.some(pokemon => pokemonRoles[pokemon.name]?.roles.includes('rainSetter'));
-    
-    if (hasRainSetter && bestModel === 'rain') {
-        console.log("Rain setter è nel team, aumento del punteggio per il modello rain.");
-        bestScore += 30; // Aggiungi un bonus significativo per il modello "rain" se un rainSetter è presente
-    }
-
-
 
 
     // Calcola la debolezza più frequente
